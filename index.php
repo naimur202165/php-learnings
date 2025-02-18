9eb74509e6b039db2b349f8e81ccbb27447a90b8
naimur <naimur202165@gmail.com>
2025-02-18 07:29:12 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php learning</title>
+</head>
+
+<body>
+    <?php
+    // There is three scoup in variables
+    // Local
+    // Global
+    // Static
+
+    echo "Hello World";
+    // Here i will create varibale
+
+    $red = "THis is a red color";
+    $name = "naimur rahman durjoy";
+    echo $red;
+    echo "naimur $name";
+    // echo 
+    ?>
+
+
+
+
+</body>
+
+</html>
